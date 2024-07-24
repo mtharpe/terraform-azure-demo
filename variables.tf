3edfefa869ab99467448f1e4b3ff37ec25183263
Michael Tharpe <mtharpe@gmail.com>
2024-07-24 18:25:31 -0400

    updated for lint

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "location" {
-  type = string
+  type        = string
   description = "The Azure location where all resources in this example should be created"
   default     = "East US"
 }
 
 variable "azure_instance_username" {
-  type = string
+  type        = string
   description = " Azure Instance login username"
 }
 variable "azure_instance_password" {
-  type = string
+  type        = string
   description = " Azure Instance login password"
 }
