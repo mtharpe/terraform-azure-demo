879dc88aa4711ceaf988aeca1af65aca8f1540bc
Michael Tharpe <mtharpe@gmail.com>
2024-07-24 18:24:07 -0400

    Updated for CircleCI

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "location" {
+  type = string
   description = "The Azure location where all resources in this example should be created"
   default     = "East US"
 }
 
 variable "azure_instance_username" {
+  type = string
   description = " Azure Instance login username"
 }
 variable "azure_instance_password" {
+  type = string
   description = " Azure Instance login password"
 }
